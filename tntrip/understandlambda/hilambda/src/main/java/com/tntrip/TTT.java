c688ee181052424acaf3f2bc909203a724692a65
marvin <marvin@rmbp>
2018-07-10 22:53:41 +0800

    clientID和subscriptionName 结合到一起保持唯一即可

---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/TTT.java
@@ -0,0 +1,50 @@
+package com.tntrip;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
+import java.util.function.Function;
+
+/**
+ * Created by gejinfeng on 2017/12/2.
+ sos
+ * @author gejinfeng
+ * @date 2017/12/2
+ */
+public class TTT {
+
+    private Map<Integer, Integer> cache = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        Semaphore s = new Semaphore(10);
+        while (true){
+        try {
+            s.acquire();
+            System.out.println(s.availablePermits());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }}
+
+    }
+
+    public int fibonaacci(Integer i) {
+        System.out.println(".....");
+        if (i == 0 || i == 1) {
+            return i;
+        }
+        // Java 8 Map接口中新增方法
+        // 首先判断缓存MAP中是否存在指定key的值，如果不存在，会自动调用mappingFunction(key)
+        // 计算key的value，然后将key = value
+        // 放入到缓存Map,java8会使用thread-safe的方式从cache中存取记录。
+        return cache.computeIfAbsent(i, new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer key) {
+                // 函数式接口,key = i,就是传过来的key,函数中的第一个参数
+                System.out.println("Compute fibonaacci " + key);
+                return TTT.this.fibonaacci(key - 1) + TTT.this.fibonaacci(key - 2);
+            }
+        });
+    }
+
+}

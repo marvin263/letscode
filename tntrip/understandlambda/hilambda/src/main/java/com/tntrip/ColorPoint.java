d4d574062284ef822c478fefb23b3c0fa67160df
marvin263 <marvin263@163.com>
2018-08-03 16:42:01 +0800

    d

---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/ColorPoint.java
@@ -0,0 +1,39 @@
+package com.tntrip;
+
+class Point {
+    private final int x, y;
+    private final String name; // Cached at construction time
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        name = makeName();
+    }
+
+    protected String makeName() {
+        return "[" + x + "," + y + "]";
+    }
+
+    public final String toString() {
+        return name;
+    }
+}
+
+
+public class ColorPoint extends Point {
+    private final String color;
+
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    protected String makeName() {
+        return super.makeName() + ":" + color;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ColorPoint(4, 2, "purple"));
+    }
+}
+

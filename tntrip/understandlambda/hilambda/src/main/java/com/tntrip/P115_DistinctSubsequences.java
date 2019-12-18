01ad556f295454eba57f24b36464c65b2c229abd
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2019-12-18 09:09:34 +0800

    ddd

---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P115_DistinctSubsequences.java
@@ -0,0 +1,13 @@
+package com.tntrip;
+
+public class P115_DistinctSubsequences {
+    public int numDistinct(String s, String t) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        P115_DistinctSubsequences p = new P115_DistinctSubsequences();
+        System.out.println(p.numDistinct("", ""));
+    }
+
+}

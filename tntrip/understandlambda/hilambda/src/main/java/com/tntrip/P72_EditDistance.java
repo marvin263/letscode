d2d0fdc01d9971def585b27214ee55e15e3d4151
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2019-12-25 01:40:41 +0800

    dd

---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P72_EditDistance.java
@@ -0,0 +1,67 @@
+package com.tntrip;
+
+public class P72_EditDistance {
+    public enum Action {
+        REPLACE {
+            @Override
+            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
+                showProgress("Before: ", curPos, curSb, target);
+                // 删除最后一个
+                curSb.deleteCharAt(curSb.length() - 1);
+                showProgress("After: ", curPos, curSb, target);
+                return new int[]{curPos[0] - 1, curPos[1] - 1};
+            }
+        },
+        DELETE {
+            @Override
+            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
+                showProgress("Before: ", curPos, curSb, target);
+                // 删除最后一个
+                curSb.deleteCharAt(curSb.length() - 1);
+                showProgress("After: ", curPos, curSb, target);
+                return new int[]{curPos[0] - 1, curPos[1]};
+            }
+        },
+        INSERT {
+            @Override
+            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
+                showProgress("Before: ", curPos, curSb, target);
+                // 插入
+                curSb.insert(curPos[0], target.charAt(curPos[1]));
+                showProgress("After: ", curPos, curSb, target);
+                return new int[]{curPos[0], curPos[1]};
+            }
+        },
+        SKIP {
+            @Override
+            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
+                showProgress("Before: ", curPos, curSb, target);
+                // 删除最后一个
+                curSb.deleteCharAt(curSb.length() - 1);
+                showProgress("After: ", curPos, curSb, target);
+                return new int[]{curPos[0] - 1, curPos[1] - 1};
+            }
+        };
+
+        public abstract int[] prevPosition(int[] curPos, StringBuilder curSb, String target);
+
+        public void showProgress(String note, int[] curPos, StringBuilder curSb, String target) {
+            System.out.println(note + "curPos=[" + curPos[0] + "," + curPos[1] + "], curSb=" + curSb.toString() + ", curAction = " + this);
+        }
+    }
+
+    public static class EachStep {
+        int doneActionCount;
+        Action lastAction;
+    }
+
+    public int minDistance(String word1, String word2) {
+        EachStep[][] dp = new EachStep[word1.length() + 1][word2.length() + 1];
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        P72_EditDistance p = new P72_EditDistance();
+        System.out.println(p.minDistance("", ""));
+    }
+}

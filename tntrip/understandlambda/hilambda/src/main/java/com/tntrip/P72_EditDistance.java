ef8424fd2238e658fa48412eecdc374fee65dd51
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2019-12-26 00:05:18 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P72_EditDistance.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P72_EditDistance.java
@@ -4,72 +4,83 @@ public class P72_EditDistance {
     public enum Action {
         REPLACE {
             @Override
-            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
-                showProgress("Before: ", curPos, curSb, target);
-                // 删除最后一个
-                curSb.deleteCharAt(curSb.length() - 1);
-                showProgress("After: ", curPos, curSb, target);
-                return new int[]{curPos[0] - 1, curPos[1] - 1};
+            public int[] prevPosition(String target, StringBuilder curFrom, int[] curPos) {
+                int i = curPos[0], j = curPos[1];
+                String before = curFrom.toString();
+                curFrom.replace(i - 1, i, target.charAt(j - 1) + "");
+                String after = curFrom.toString();
+                String info = String.format("target=%s, action=%s, %s-->%s", target, this, before, after);
+                System.out.println(info);
+                return new int[]{i - 1, j - 1};
             }
         },
-        REMOVE {
+        DELETE {
             @Override
-            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
-                showProgress("Before: ", curPos, curSb, target);
-                // 删除最后一个
-                curSb.deleteCharAt(curSb.length() - 1);
-                showProgress("After: ", curPos, curSb, target);
-                return new int[]{curPos[0] - 1, curPos[1]};
+            public int[] prevPosition(String target, StringBuilder curFrom, int[] curPos) {
+                int i = curPos[0], j = curPos[1];
+                String before = curFrom.toString();
+                curFrom.deleteCharAt(i - 1);
+                String after = curFrom.toString();
+                String info = String.format("target=%s, action=%s, %s-->%s", target, this, before, after);
+                System.out.println(info);
+                return new int[]{i - 1, j};
             }
         },
         INSERT {
             @Override
-            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
-                showProgress("Before: ", curPos, curSb, target);
-                // 插入
-                curSb.insert(curPos[0], target.charAt(curPos[1]));
-                showProgress("After: ", curPos, curSb, target);
-                return new int[]{curPos[0], curPos[1]};
+            public int[] prevPosition(String target, StringBuilder curFrom, int[] curPos) {
+                int i = curPos[0], j = curPos[1];
+                String before = curFrom.toString();
+                curFrom.insert(i, target.charAt(j - 1));
+                String after = curFrom.toString();
+                String info = String.format("target=%s, action=%s, %s-->%s", target, this, before, after);
+                System.out.println(info);
+                return new int[]{i, j - 1};
             }
         },
         SKIP {
             @Override
-            public int[] prevPosition(int[] curPos, StringBuilder curSb, String target) {
-                showProgress("Before: ", curPos, curSb, target);
-                // 删除最后一个
-                curSb.deleteCharAt(curSb.length() - 1);
-                showProgress("After: ", curPos, curSb, target);
-                return new int[]{curPos[0] - 1, curPos[1] - 1};
+            public int[] prevPosition(String target, StringBuilder curFrom, int[] curPos) {
+                String before = curFrom.toString();
+                String after = curFrom.toString();
+                String info = String.format("target=%s, action=%s, %s-->%s", target, this, before, after);
+                //System.out.println(info);
+                int i = curPos[0], j = curPos[1];
+                return new int[]{i - 1, j - 1};
             }
         };
 
-        public abstract int[] prevPosition(int[] curPos, StringBuilder curSb, String target);
+        public abstract int[] prevPosition(String target, StringBuilder curFrom, int[] curPos);
 
-        public void showProgress(String note, int[] curPos, StringBuilder curSb, String target) {
-            System.out.println(note + "curPos=[" + curPos[0] + "," + curPos[1] + "], curSb=" + curSb.toString() + ", curAction = " + this);
-        }
     }
 
     public static class EachStep {
-        int doneActionCount;
-        Action lastAction;
+        /**
+         * 加上当前的action，总共的action数量
+         */
+        final int alreadyDoneCount;
+        final Action action;
+
+        private EachStep(final int alreadyDoneCount, final Action action) {
+            this.alreadyDoneCount = alreadyDoneCount;
+            this.action = action;
+        }
 
         public static EachStep create(int doneActionCount, Action lastAction) {
-            EachStep es = new EachStep();
-            es.doneActionCount = doneActionCount;
-            es.lastAction = lastAction;
-            return es;
+            return new EachStep(doneActionCount, lastAction);
         }
     }
 
-    public int minDistance(String word1, String word2) {
+    public EachStep[][] minDistance(String word1, String word2) {
         EachStep[][] dp = new EachStep[word1.length() + 1][word2.length() + 1];
         dp[0][0] = EachStep.create(0, Action.SKIP);
+        // word1 太短了，给word1插入字符
         for (int i = 1; i < (word2.length() + 1); i++) {
             dp[0][i] = EachStep.create(i, Action.INSERT);
         }
+        // word1 太长了，从word1中删除字符
         for (int i = 1; i < (word1.length() + 1); i++) {
-            dp[i][0] = EachStep.create(i, Action.REMOVE);
+            dp[i][0] = EachStep.create(i, Action.DELETE);
         }
         for (int m = 0; m < word1.length(); m++) {
             char c1 = word1.charAt(m);
@@ -78,33 +89,60 @@ public class P72_EditDistance {
                 int j = n + 1;
                 char c2 = word2.charAt(n);
                 if (c1 == c2) {
-                    dp[i][j] = EachStep.create(dp[i - 1][j - 1].doneActionCount, Action.SKIP);
+                    dp[i][j] = EachStep.create(dp[i - 1][j - 1].alreadyDoneCount, Action.SKIP);
                 } else {
                     dp[i][j] = chooseMin(
-                            EachStep.create(dp[i][j - 1].doneActionCount + 1, Action.INSERT),
-                            EachStep.create(dp[i - 1][j].doneActionCount + 1, Action.REMOVE),
-                            EachStep.create(dp[i - 1][j - 1].doneActionCount + 1, Action.REPLACE));
+                            // 知道了si-->sj-1：那么si怎么到sj呢？
+                            // si可以变为sj-1，为了让si变为sj，s[i]后面再插入一个字母（即si变为si+1）就可以了，即：si 多插入一个字符
+                            EachStep.create(dp[i][j - 1].alreadyDoneCount + 1, Action.INSERT),
+                            // 知道了si-1-->sj：那么si怎么到sj呢？要si多删除一个字母（即si变为si-1）就可以了，即：si 多删除一个字符
+                            EachStep.create(dp[i - 1][j].alreadyDoneCount + 1, Action.DELETE),
+                            // 知道了si-1-->sj-1：那么si怎么到sj呢？给s[i]赋值为s[j]就可以了，即：si 被替换一个字符
+                            EachStep.create(dp[i - 1][j - 1].alreadyDoneCount + 1, Action.REPLACE)
+                    );
                 }
             }
         }
-
-
-        return dp[word1.length()][word2.length()].doneActionCount;
+        return dp;
     }
 
     private EachStep chooseMin(EachStep... steps) {
         EachStep min = steps[0];
         for (int i = 1; i < steps.length; i++) {
-            min = (min.doneActionCount > steps[i].doneActionCount) ? steps[i] : min;
+            min = (min.alreadyDoneCount > steps[i].alreadyDoneCount) ? steps[i] : min;
         }
         return min;
     }
 
+    public void showProgress(String from, String target) {
+        StringBuilder curFrom = new StringBuilder(from);
+
+        EachStep[][] dp = minDistance(from, target);
+
+        int[] curPos = new int[]{from.length(), target.length()};
+        EachStep curEs = dp[curPos[0]][curPos[1]];
+
+        while (curEs.alreadyDoneCount > 0) {
+            int[] prevPos = curEs.action.prevPosition(target, curFrom, curPos);
+            if (prevPos == null) {
+                break;
+            }
+            EachStep prevEs = dp[prevPos[0]][prevPos[1]];
+
+            curPos = prevPos;
+            curEs = prevEs;
+        }
+    }
+
     public static void main(String[] args) {
         P72_EditDistance p = new P72_EditDistance();
-        // 3
-        System.out.println(p.minDistance("horse", "ros"));
-        // 5
-        System.out.println(p.minDistance("intention", "execution"));
+
+        p.showProgress("horse", "ros");
+
+        p.showProgress("intention", "execution");
+
+        String word1 = "议传达学央农村工作会议在习了习近平总书记日前在中央";
+        String word2 = "议在议传达农村工习近平总书记日作会习了前在中央";
+        p.showProgress(word1, word2);
     }
 }

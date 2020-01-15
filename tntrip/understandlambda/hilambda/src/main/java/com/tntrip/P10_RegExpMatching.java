ce4dfccdc51dcb2a94499ce9c3ef9176f62db20c
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2020-01-15 09:46:28 +0800

    ddd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P10_RegExpMatching.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P10_RegExpMatching.java
@@ -21,15 +21,19 @@ public class P10_RegExpMatching {
         // 是否匹配
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
-
-
+        //"" 和p的匹配关系初始化，a*a*a*a*a*这种能够匹配空串，其他的是都是false。
+        //  奇数位不管什么字符都是false，偶数位为* 时则: dp[0][i] = dp[0][i - 2]
+        for (int i = 2; i <= p.length(); i += 2) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
         char[] chars1 = s.toCharArray();
         char[] chars2 = p.toCharArray();
-        for (int i = 1; i < chars1.length; i++) {
+        for (int i = 1; i <= chars1.length; i++) {
             char c1 = chars1[i - 1];
-            for (int j = 1; j < chars2.length; j++) {
+            for (int j = 1; j <= chars2.length; j++) {
                 char c2 = chars2[j - 1];
-
                 if (c1 == c2) {
                     dp[i][j] = dp[i - 1][j - 1];
                 }
@@ -38,24 +42,28 @@ public class P10_RegExpMatching {
                     if (c2 == '.') {
                         dp[i][j] = dp[i - 1][j - 1];
                     } else if (c2 == '*') {
-                        dp[i][j] =
-                                dp[i][j - 1] || //匹配了1次
-                                        dp[i][j - 2] || //匹配了0次
-                                        dp[i - 1][j - 1] ||//匹配了0次
-                                        dp[i - 1][j - 1] ||//匹配了0次
-                                        dp[i - 1][j - 1] //匹配了0次
-                        ;
-                    } else {
+                        char c3 = chars2[j - 2];
+                        if (c1 == c3 || c3 == '.') {
+                            dp[i][j] =
+                                    dp[i][j - 1] || //匹配了1次
+                                            dp[i - 1][j] ||// 匹配了多次
+                                            dp[i][j - 2]
+                            ;
+                        } else {
+                            dp[i][j] =
+                                    dp[i][j - 2]
+                            ;
+                        }
 
+                    } else {
+                        dp[i][j] = false;
                     }
                 }
-
-
             }
         }
 
 
-        return false;
+        return dp[s.length()][p.length()];
     }
 
 
---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/P968_BinaryTreeCameras.java
@@ -0,0 +1,70 @@
+package com.tntrip;
+
+public class P968_BinaryTreeCameras {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int minCameraCover(TreeNode root) {
+        return -1;
+    }
+
+    private TreeNode toTree(Integer[] intValues) {
+        if (intValues.length == 0) {
+            return null;
+        }
+        if (intValues[0] == null) {
+            throw new RuntimeException("Root node value should not be null");
+        }
+        int last = intValues.length - 1;
+        int mid = intValues.length / 2;
+        TreeNode[] nodes = new TreeNode[mid + 1];
+        nodes[0] = new TreeNode(intValues[0]);
+        for (int i = 0; i <= mid; i++) {
+            int left = 2 * i + 1;
+            if (left <= last && intValues[left] != null) {
+                nodes[i].left = new TreeNode(intValues[left]);
+            }
+            int right = 2 * i + 2;
+            if (right <= last && intValues[right] != null) {
+                nodes[i].right = new TreeNode(intValues[right]);
+            }
+
+            if (left <= mid) {
+                nodes[left] = nodes[i].left;
+            }
+            if (right <= mid) {
+                nodes[right] = nodes[i].right;
+            }
+        }
+        return nodes[0];
+    }
+    // 广度优先构建
+    private TreeNode bfs(Integer[] intValues) {
+        if (intValues.length == 0) {
+            return null;
+        }
+        if (intValues[0] == null) {
+            throw new RuntimeException("Root node value should not be null");
+        }
+        TreeNode root = new TreeNode(intValues[0]);
+        TreeNode[] nodes = new TreeNode[mid + 1];
+        nodes[0] = new TreeNode(intValues[0]);
+    }
+
+    public static void main(String[] args) {
+        P968_BinaryTreeCameras p = new P968_BinaryTreeCameras();
+        //Integer[] intValues1 = new Integer[]{0, 0, null, 0, 0};
+        Integer[] intValues2 = new Integer[]{0, 0, null, 0, null, 0, null, null, 0};
+        //TreeNode treeNode1 = p.toTree(intValues1);
+        TreeNode treeNode2 = p.toTree(intValues2);
+        //System.out.println(treeNode1.toString() + treeNode2.toString());
+    }
+
+}

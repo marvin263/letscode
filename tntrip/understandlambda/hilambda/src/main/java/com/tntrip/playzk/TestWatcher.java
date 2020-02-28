deeb05285ead485bb6e17b65c074478f448ba54f
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2020-02-28 08:38:38 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/playzk/TestWatcher.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/playzk/TestWatcher.java
@@ -22,12 +22,12 @@ public class TestWatcher {
     private static final int SESSION_TIMEOUT_IN_MILLS = 40000 * 5;
 
     private ZooKeeper zk;
-    private String note;
     private int processId;
-    private static AtomicLong ZKROUND = new AtomicLong(0);
+    private static volatile AtomicLong ZKROUND = new AtomicLong(0);
 
-    private static AtomicLong GLOBAL = new AtomicLong(0);
-    private static AtomicLong EXPLICIT = new AtomicLong(0);
+    private static volatile AtomicLong EXPIRE = new AtomicLong(0);
+    private static volatile AtomicLong GLOBAL = new AtomicLong(0);
+    private static volatile AtomicLong EXPLICIT = new AtomicLong(0);
 
     public TestWatcher() throws Exception {
         processId = TnStringUtils.getProcessID();
@@ -41,6 +41,8 @@ public class TestWatcher {
         }
         zk = new ZooKeeper(CONNECT_STRING, SESSION_TIMEOUT_IN_MILLS, new GlobalWatcher());
         System.out.println("Create new ZooKeeper instance. ZKROUND=" + ZKROUND.incrementAndGet());
+
+        EXPIRE.set(0L);
         GLOBAL.set(0L);
         EXPLICIT.set(0L);
         setWatches(zk);
@@ -48,16 +50,18 @@ public class TestWatcher {
     }
 
     private void setWatches(ZooKeeper zk) throws Exception {
+        zk.exists("/dummy", new ExpireWatcher());
+
         ExplicitWatcher ew = new ExplicitWatcher();
         String str2 = "/explicitWatcher";
-        
+
         long b1 = System.currentTimeMillis();
         System.out.println("explicitWatcher begin");
         if (zk.exists(str2, false) == null) {
             zk.create(str2, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }
         System.out.println("explicitWatcher end, cost: " + (System.currentTimeMillis() - b1));
-        
+
         zk.getChildren(str2, ew);
         zk.getData(str2, ew, new Stat());
 
@@ -65,7 +69,6 @@ public class TestWatcher {
         if (zk.exists(str1, false) == null) {
             zk.create(str1, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }
-
         long b2 = System.currentTimeMillis();
         System.out.println("globalWatcher begin");
         zk.getChildren(str1, true);
@@ -78,15 +81,6 @@ public class TestWatcher {
         public void process(WatchedEvent event) {
             GLOBAL.incrementAndGet();
             LOG.info("GlobalWatcher: " + event + ". " + readableContext());
-            // expire就得创建 ZooKeeper实例
-            if (event.getState() == Event.KeeperState.Expired) {
-                try {
-                    createZK();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
             // path上能设置事件，那就设置上事件 
             String path = event.getPath();
             if (path != null && event.getType() != Event.EventType.NodeDeleted) {
@@ -100,11 +94,28 @@ public class TestWatcher {
         }
     }
 
+    public class ExpireWatcher implements Watcher {
+        @Override
+        public void process(WatchedEvent event) {
+            EXPIRE.incrementAndGet();
+            LOG.info("ExpireWatcher: " + event + ". " + readableContext());
+            // expire就得创建 ZooKeeper实例
+            if (event.getState() == Event.KeeperState.Expired) {
+                try {
+                    createZK();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
     private String readableContext() {
-        return TnStringUtils.format("Process--{0}, zkRound={1}, sessionId={2}, GlobalWatcherRound={3}, ExplicitWatcherRound={4}",
+        return TnStringUtils.format("Process--{0}, zkRound={1}, sessionId={2}, Expire={3}, Global={4}, Explicit={5}",
                 processId + "",
                 ZKROUND.get() + "",
                 zk.getSessionId() + "",
+                EXPIRE.get() + "",
                 GLOBAL.get() + "",
                 EXPLICIT.get() + "");
     }
@@ -114,14 +125,6 @@ public class TestWatcher {
         public void process(WatchedEvent event) {
             EXPLICIT.incrementAndGet();
             LOG.info("ExplicitWatcher: " + event + ". " + readableContext());
-            // expire就得创建 ZooKeeper实例
-            if (event.getState() == Event.KeeperState.Expired) {
-                try {
-                    createZK();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
             // path上能设置事件，那就设置上事件 
             String path = event.getPath();
             if (path != null && event.getType() != Event.EventType.NodeDeleted) {

ea99310bd065f52cad495f2a3b046a2311ad35ad
libing2 <libing2@tuniu.com>
2020-04-14 18:14:00 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/BBB.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/BBB.java
@@ -1,10 +1,12 @@
 package com.tntrip;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Queue;
 import java.util.TreeMap;
 
 public class BBB {
@@ -49,6 +51,79 @@ public class BBB {
         return c;
     }
 
+    public static class Node {
+        // 0-based
+        public int col = 0;
+        // 0-based
+        public int level = 0;
+        public final int value;
+        public Node left;
+        public Node right;
+
+        public static Node create(final int value) {
+            Node n = new Node(value);
+            return n;
+        }
+
+        public Node(final int value) {
+            this.value = value;
+        }
+
+
+        public void appendLeft(Node left) {
+            this.left = left;
+            left.level = this.level + 1;
+            left.col = this.col;
+        }
+
+        public void appendRight(Node right) {
+            this.right = right;
+            right.level = this.level + 1;
+            right.col = this.col + 1;
+        }
+
+        public void incrCol() {
+            col++;
+        }
+
+        public void incrLevel() {
+            col++;
+        }
+    }
+
+    public static void ddd(int d) {
+        int nodeCount = 64;
+        int initialValue = nodeCount / 2;
+        int step = initialValue / 2;
+
+        List<Integer> initial = Collections.singletonList(initialValue);
+
+
+        List<String> curLevel = new ArrayList<>();
+        for (int i = nodeCount / 2; i >= 1; i++) {
+            Node n = Node.create(i + "");
+        }
+    }
+
+    private Queue<Node> calcNextLevel(Queue<Node> queue) {
+        Queue<Node> rstQueue = new ArrayDeque<>();
+        int step = queue.peek().value / 2;
+        while (!queue.isEmpty()) {
+            Node n = queue.remove();
+
+            Node left = Node.create(n.value - step);
+            n.appendLeft(left);
+
+            Node right = Node.create(n.value + step);
+            n.appendRight(right);
+        }
+        List<Integer> level = new ArrayList<>(prevLevel.size() * 2);
+        for (Integer v : prevLevel) {
+            level.add(v - step);
+            level.add(v + step);
+        }
+        return level;
+    }
 
     public static void main(String[] args) {
         BBB b = new BBB();

80f07dd8d9665ea1b2a3aec715e28be0d99ee4b8
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2020-07-08 23:15:53 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/ThreadLocalTest.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/ThreadLocalTest.java
@@ -1,9 +1,23 @@
 package com.tntrip;
 
+import com.tntrip.playzk.Executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadLocalTest {
     public static final ThreadLocal<String> itl = new InheritableThreadLocal<>();
 
     public static void main(String[] args) throws Exception {
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        es.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("dddddd");
+            }
+        });
+        es.awaitTermination(20, TimeUnit.HOURS);
         itl.set(Thread.currentThread().getName());
         System.out.println(itl.get());
         Thread t = new Thread("second-thread") {
@@ -18,3 +32,4 @@ public class ThreadLocalTest {
 
 
 }
+

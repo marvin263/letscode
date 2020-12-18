d88e2e1d28280d8fef017754fbd579ac1047a53b
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2020-12-18 23:55:59 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/focus/trie/P0720_LongestWordInDictionary.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/focus/trie/P0720_LongestWordInDictionary.java
@@ -1,8 +1,12 @@
-package com.tntrip.focus.tire;
+package com.tntrip.focus.trie;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class P0720_LongestWordInDictionary {
     public static class TrieNode {
@@ -42,7 +46,7 @@ public class P0720_LongestWordInDictionary {
 
             List<TrieNode> list = new ArrayList<>();
             for (Cell cell : cells) {
-                if (cell != null && cell.asTerminatorCount > 0) {
+                if (cell != null && cell.asTerminatorCount > 0 && nodeContainsWordTerminator(cell.child)) {
                     list.add(cell.child);
                 }
             }
@@ -93,7 +97,7 @@ public class P0720_LongestWordInDictionary {
      * @param n
      * @return
      */
-    private boolean nodeContainsWordTerminator(TrieNode n) {
+    private static boolean nodeContainsWordTerminator(TrieNode n) {
         if (n == null || n.cells == null) {
             return false;
         }
@@ -118,6 +122,7 @@ public class P0720_LongestWordInDictionary {
             if (nextLevelIsEmpty(nextLevel)) {
                 break;
             }
+
             curLevel.clear();
             curLevel.addAll(nextLevel);
         }
@@ -152,7 +157,7 @@ public class P0720_LongestWordInDictionary {
                 break;
             }
         }
-        
+
         if (whichCell == -1) {
             throw new RuntimeException("Should never been here");
         }
@@ -176,11 +181,51 @@ public class P0720_LongestWordInDictionary {
         return s;
     }
 
+    public String longestWord1(String[] words) {
+        Comparator<String> s = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()) {
+                    return s2.compareTo(s1);
+                }
+                return s1.length() - s2.length();
+            }
+        };
+
+        Set<String> set = Arrays.stream(words).collect(Collectors.toSet());
+
+        Arrays.sort(words, s);
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            String word = words[i];
+
+            int idx = -1;
+            for (idx = word.length() - 1; idx >= 1; idx--) {
+                if (set.contains(word.substring(0, idx))) {
+
+                } else {
+                    break;
+                }
+            }
+
+            if (idx == 0) {
+                return word;
+            }
+        }
+
+        return "";
+    }
+
+
     public static void main(String[] args) {
         P0720_LongestWordInDictionary p = new P0720_LongestWordInDictionary();
-        // "yyd"
-        String[] words = new String[]{"n", "j", "sl", "yyd", "slo", "jk", "jkdt", "y", "yy"};
+        // "world"
+        String[] words = new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"};
         System.out.println(p.longestWord(words));
+
+        // "yyd"
+        String[] words1 = new String[]{"n", "j", "sl", "yyd", "slo", "jk", "jkdt", "y", "yy"};
+        System.out.println(p.longestWord(words1));
 
         // apple
         //String[] words2 = new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"};

cf27c311f2050d733e2a6a096cf4858fe4045274
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2020-12-19 16:40:45 +0800

    dd

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/focus/trie/P0720_LongestWordInDictionary.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/focus/trie/P0720_LongestWordInDictionary.java
@@ -1,184 +1,97 @@
 package com.tntrip.focus.trie;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class P0720_LongestWordInDictionary {
     public static class TrieNode {
-        public TrieNode parent;
-        public int parentCellIdx;
-        public Cell[] cells;
+        private boolean wordTerminator;
+        public TrieNode[] children;
 
-        public TrieNode(TrieNode parent, int parentCellIdx) {
-            this.parent = parent;
-            this.parentCellIdx = parentCellIdx;
-        }
-
-        public TrieNode setChar(char c, boolean terminatorC) {
-            cells = cells == null ? new Cell[26] : cells;
-
+        public TrieNode setChar(char c, boolean wordTerminator) {
+            children = children == null ? new TrieNode[26] : children;
             int idx = toIdx(c);
-            cells[idx] = cells[idx] == null ? new Cell(idx) : cells[idx];
-
-            if (terminatorC) {
-                cells[idx].asTerminatorCount++;
+            children[idx] = children[idx] == null ? new TrieNode() : children[idx];
+            if (wordTerminator) {
+                children[idx].wordTerminator = true;
             }
-            return cells[idx].child;
+            return children[idx];
         }
 
-        /**
-         * 在当前这个 node 中， 某些 cell 已经是 某个词的终结符了。
-         * 这些cell下的 child节点 才 *有可能* 后续被继续使用
-         * <p>
-         * 注意：这些child节点本身，可能并不包含词的终结符，所以是rawCandidate
-         *
-         * @return
-         */
-        public List<TrieNode> rawCandidates() {
-            if (cells == null) {
-                return Collections.emptyList();
-            }
-
-            List<TrieNode> list = new ArrayList<>();
-            for (Cell cell : cells) {
-                if (cell != null && cell.asTerminatorCount > 0 && nodeContainsWordTerminator(cell.child)) {
-                    list.add(cell.child);
-                }
-            }
-            return list;
+        public boolean containsChar(char c) {
+            return children != null && children[toIdx(c)] != null;
         }
 
-        public int toIdx(char c) {
-            return (int) c - (int) 'a';
+        public boolean charIsWordTerminator(char c) {
+            return containsChar(c) && children[toIdx(c)].wordTerminator;
         }
 
-        public char toChar(int idx) {
-            return (char) ((int) 'a' + idx);
+        public TrieNode getNode(char c) {
+            return children == null ? null : children[toIdx(c)];
         }
 
-        public class Cell {
-            public int asTerminatorCount = 0;
-            public TrieNode child;
-
-            public Cell(int cellIdx) {
-                child = new TrieNode(TrieNode.this, cellIdx);
-            }
-        }
-    }
-
-    public TrieNode buildTree(String[] words) {
-        TrieNode root = new TrieNode(null, 0);
-
-        for (String str : words) {
-            TrieNode targetNode = root;
-
-            char[] chars = str.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                targetNode = targetNode.setChar(chars[i], i == chars.length - 1);
-            }
-        }
-        return root;
-    }
-
-    public String findLongestWord(String[] words) {
-        TrieNode root = buildTree(words);
-        TrieNode deepestNode = deepestNode(root);
-        return buildLongestWord(deepestNode);
-    }
-
-    /**
-     * 该node中的某个cell确实是 某个word的终结符
-     *
-     * @param n
-     * @return
-     */
-    private static boolean nodeContainsWordTerminator(TrieNode n) {
-        if (n == null || n.cells == null) {
-            return false;
-        }
-        for (TrieNode.Cell cell : n.cells) {
-            if (cell != null && cell.asTerminatorCount > 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private TrieNode deepestNode(TrieNode root) {
-        List<TrieNode> curLevel = new ArrayList<>();
-        curLevel.add(root);
-
-        List<TrieNode> nextLevel = new ArrayList<>();
-        while (true) {
-            nextLevel.clear();
-            for (TrieNode cur : curLevel) {
-                nextLevel.addAll(cur.rawCandidates());
-            }
-            if (nextLevelIsEmpty(nextLevel)) {
-                break;
-            }
-
-            curLevel.clear();
-            curLevel.addAll(nextLevel);
-        }
-
-        for (TrieNode n : curLevel) {
-            if (nodeContainsWordTerminator(n)) {
-                return n;
-            }
-        }
-        return null;
-    }
-
-    private boolean nextLevelIsEmpty(List<TrieNode> nextLevel) {
-        for (TrieNode n : nextLevel) {
-            if (nodeContainsWordTerminator(n)) {
+        public boolean existWordTerminator() {
+            if (children == null) {
                 return false;
             }
+            for (TrieNode n : children) {
+                if (n != null && n.wordTerminator) {
+                    return true;
+                }
+            }
+            return false;
         }
-        return true;
     }
 
+    public static int toIdx(char c) {
+        return (int) c - (int) 'a';
+    }
 
-    public String buildLongestWord(TrieNode n) {
-        if (n == null) {
-            return "";
-        }
-
-        int whichCell = -1;
-        for (int i = 0; i < n.cells.length; i++) {
-            if (n.cells[i] != null && n.cells[i].asTerminatorCount > 0) {
-                whichCell = i;
-                break;
-            }
-        }
-
-        if (whichCell == -1) {
-            throw new RuntimeException("Should never been here");
-        }
-
-        List<Character> list = new ArrayList<>();
-        while (n != null) {
-            list.add(n.toChar(whichCell));
-            whichCell = n.parentCellIdx;
-            n = n.parent;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = list.size() - 1; i >= 0; i--) {
-            sb.append(list.get(i));
-        }
-        return sb.toString();
+    public static char toChar(int idx) {
+        return (char) ((int) 'a' + idx);
     }
 
     public String longestWord(String[] words) {
-        String s = findLongestWord(words);
-        return s;
+        TrieNode root = new TrieNode();
+        TrieNode curNode = null;
+        TrieNode nextNode = null;
+        for (String str : words) {
+            curNode = root;
+            char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                nextNode = curNode.setChar(chars[i], i == chars.length - 1);
+                curNode = nextNode;
+            }
+        }
+
+        String strLongest = "";
+        for (String str : words) {
+            char[] chars = str.toCharArray();
+            curNode = root;
+            boolean legal = true;
+            for (int i = 0; i < chars.length; i++) {
+                char c = chars[i];
+                TrieNode n = curNode.getNode(c);
+                curNode = n;
+                if (!n.wordTerminator) {
+                    legal = false;
+                    break;
+                }
+            }
+            
+            if (legal) {
+                if (str.length() > strLongest.length()) {
+                    strLongest = str;
+                } else if (str.length() == strLongest.length()) {
+                    if (str.compareTo(strLongest) < 0) {
+                        strLongest = str;
+                    }
+                }
+            }
+        }
+        return strLongest;
     }
 
     public String longestWord1(String[] words) {

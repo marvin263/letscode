af068575625971cb2b74aa5bc62e3f5c93a47864
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2021-05-06 10:31:03 +0800

    mq

---
+++ tntrip/spring-amqp-samples-master/spring-rabbit-json/src/test/java/org/springframework/amqp/samples/json/ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.springframework.amqp.samples.json;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

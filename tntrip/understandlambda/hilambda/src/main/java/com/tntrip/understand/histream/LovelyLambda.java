6a18063f51e482eca0bc1e8b39a0a3415465b860
marvin263 <marvin263@DESKTOP-JQH6SAM.com>
2021-06-20 16:16:37 +0800

    不止是Stream，还得学习Stream中的特殊reduce，也就是collect。collect又抽象出了Collector

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/histream/LovelyLambda.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/histream/LovelyLambda.java
@@ -96,7 +96,7 @@ public class LovelyLambda {
     private static List<Order> createOrders() {
         List<Order> orders = new ArrayList<>();
         Random rdm = new SecureRandom();
-        int ordCount = 6;
+        int ordCount = 500;
         int eachItemCount = 3;
 
         for (int i = 0; i < ordCount; i++) {
@@ -125,6 +125,7 @@ public class LovelyLambda {
                 .flatMap(e -> e.items.stream())
                 .collect(Collectors.toList());
 
+        long b1 = System.currentTimeMillis();
         Map<Boolean, List<OrderItem>> attrMap = orderItems.stream()
                 .map(a -> (OrderItem) a)
                 .collect(Collectors.groupingBy(
@@ -141,13 +142,34 @@ public class LovelyLambda {
                 .collect(Collectors.partitioningBy(c -> c.parentOrdId > 2));
         List<OrderItem> hasAttr = attrMap.get(true);
         List<OrderItem> noAttr = attrMap.get(false);
-        System.out.println();
+
+        long b2 = System.currentTimeMillis();
+        Map<Boolean, List<OrderItem>> attrMap2 = orderItems.stream()
+                .map(a -> (OrderItem) a)
+                .collect(Collectors.groupingBy(
+                        OrderItem::getProductName,
+                        LinkedHashMap::new,
+                        Collectors.groupingBy(
+                                OrderItem::getItemId,
+                                Collectors.collectingAndThen(
+                                        Collectors.toList(),
+                                        e -> e.stream().max(Comparator.comparingInt(OrderItem::getItemId)).get())  //寻找合适的wishlist_id
+                        )))
+                .values()
+                .stream()
+                .flatMap(b -> b.values().stream())
+                .filter(LovelyLambda::filterKeyAndGoodsId)
+                .collect(Collectors.partitioningBy(c -> c.parentOrdId > 2));
+        List<OrderItem> hasAttr2 = attrMap2.get(true);
+        List<OrderItem> noAttr2 = attrMap2.get(false);
+
+        System.out.println(b2 - b1);
+        System.out.println(System.currentTimeMillis() - b2);
     }
 
     private static boolean filterKeyAndGoodsId(OrderItem d) {
         return true;
     }
-
 
     private static void testFlatMap() {
         List<Order> orders = createOrders();
